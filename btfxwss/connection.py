add58af63e7ee461a586931639af1fea5ad01846
eSanity <richard.-hoekstra@hotmail.com>
2017-11-03 15:14:58 +0100

    Re-adding the payload jsonification

--- btfxwss/connection.py
+++ btfxwss/connection.py
@@ -265,6 +265,7 @@ class WebSocketConnection(Thread):
 
             payload = {'event': 'auth', 'apiKey': api_key, 'authSig': auth_sig,
                        'authPayload': auth_string, 'authNonce': nonce}
+            payload = json.dumps(payload)
         elif list_data:
             payload = json.dumps(list_data)
         else:
